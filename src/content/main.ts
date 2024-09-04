e0c93de100f6059e8e46f892d1bc791fa6d959b8
stray-code <abc1007020@gmail.com>
2024-09-04 23:33:37 +0900

    スタイルをcss modulesに変更

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,10 +1,16 @@
 import { getLocalStorage } from "../utils";
 
+import classes from "./style.module.css";
+
 const init = async () => {
   const isHide = await getLocalStorage("isHide");
 
   if (isHide) {
-    import("./style.css");
+    // dynamic importでCSSを反映したいが、buildエラーになるため、CSSモジュールを使用
+    const body = document.querySelector("body");
+
+    body?.classList.add(classes.item);
+
     return;
   }
 };
