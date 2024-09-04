a0e7f9ddca33c7dcc7a277ca8e5f936729ec42a9
stray-code <abc1007020@gmail.com>
2024-09-04 22:50:45 +0900

    非表示の処理作成

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,1 +1,12 @@
-import "./style.css";
+import { getLocalStorage } from "../utils";
+
+const init = async () => {
+  const isHide = await getLocalStorage("isHide");
+
+  if (isHide) {
+    import("./style.css");
+    return;
+  }
+};
+
+init();
